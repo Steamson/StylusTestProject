1ca863ec2a85a3d6e968bcc59ca063ed37715310
DESKTOP-3QB86BB\lyuhu <lyuhunin@gmail.com>
2018-01-16 22:10:03 +0900

    Complete without Header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	Resize();
+})
+
+$(window).resize(function() {
+    Resize();
+})
+
+function Resize() {
+	if($(window).width() < 544) {
+        $('.ws-block').addClass('resize'); // даем классу col-xs новый класс new-style
+    }
+    else if ($(window).width() >= 544 && $('.ws-block').hasClass('resize')) {
+    	$('.ws-block').removeClass('resize');
+    }
+}
